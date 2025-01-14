74ce3b0bb51bc51f5734517e50191024f8187753
Chintalagiri Shashank <shashank@chintal.in>
2025-01-14 23:52:16 +0530

    Move bool into platform

--- src/hal/constants/common.h
+++ src/hal/constants/common.h
@@ -3,13 +3,7 @@
 #ifndef HAL_CONSTANTS_COMMON_H
 #define HAL_CONSTANTS_COMMON_H
 
-#define EBS_FALSE  0
-#define EBS_TRUE   1
-
-typedef enum {
-    _EBS_FALSE = EBS_FALSE,
-    _EBS_TRUE = EBS_TRUE
-} EBS_BOOL_t;
+#include <platform/constants.h>
 
 #define EBS_INTF_UART       0
 #define EBS_INTF_USBCDC     1
